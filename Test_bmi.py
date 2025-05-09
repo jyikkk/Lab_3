3b0c9b1b40e2d04838ee1ddec9a2a619e4e03497
jiayik <ljyik.24@ichat.sp.edu.sg>
2025-05-09 12:54:44 +0800

    everything new added in

---
+++ Test_bmi.py
@@ -0,0 +1,8 @@
+import Lab_2.bmi as bmi
+
+def test_bmi_normal_weight():
+    assert (bmi.calculate_bmi(1.76, 58.5) == 0)
+def test_bmi_over_weight():
+    assert (bmi.calculate_bmi(1.76, 100) == 1)
+def test_bmi_under_weight():
+    assert (bmi.calculate_bmi(1.76, 40) == -1)
